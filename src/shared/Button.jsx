66de7d079c6663780f4df2bcb256f1f87b065cdd
victory benson <victorybenson98@gmail.com>
2024-04-20 11:07:59 +0100

    minor fix

--- src/shared/Button.jsx
+++ src/shared/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Button = (props) => {
   return (
     <div>
-        <button className='bg-[#0D35FB] text-white p-2 rounded-lg hover:shadow-xl shadow-[#0D35FB] duration-200 active:scale-100 hover:bg-opacity-80'>{props.text}</button>
+        <button className='bg-[#0D35FB] text-white p-3 px-4 rounded-lg hover:shadow-xl shadow-[#0D35FB] duration-200 active:scale-100 hover:bg-opacity-80'>{props.text}</button>
     </div>
   )
 }
