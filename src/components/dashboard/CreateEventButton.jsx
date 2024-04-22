8aefb439d78b9509673abce39b1ce3b8e50e889f
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2024-04-22 11:35:57 -0700

    Update CreateEventButton.jsx

--- src/components/dashboard/CreateEventButton.jsx
+++ src/components/dashboard/CreateEventButton.jsx
@@ -1,12 +1,9 @@
-import Link from 'next/link';
-
 export default function CreateEventButton() {
     return (
-        <Link
-            href='/dashboard/onboarding/step-one'
+        <button
             className='bg-[#0D35FB] hover:bg-blue-700 text-white text-sm p-4 rounded-md'
         >
             Create Event
-        </Link>
+        </button>
     );
 }
