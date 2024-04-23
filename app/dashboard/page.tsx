43c818346043db8980c0ddbdfa3e79a82a3f2e99
Amadi Jerry <amadijerry310@gmail.com>
2024-04-23 22:57:29 +0100

    resolve conflict

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,11 +8,11 @@ const Dashboard = () => {
     const { data: session, status: sessionStatus } = useSession();
     const router = useRouter();
 
-  // Redirect to login if not authenticated
-  if (sessionStatus !== 'authenticated') {
-    router.push('/auth/login');
-    return null; // Return null while redirecting
-  }
+    // Redirect to login if not authenticated
+    if (sessionStatus !== 'authenticated') {
+        router.push('/login');
+        return null; // Return null while redirecting
+    }
 
     if (mockData.length === 0) {
         return (
