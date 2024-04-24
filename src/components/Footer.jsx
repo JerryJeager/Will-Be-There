3e25225fbb77f7f1d8f7a6a25e2f60f254fd84ce
victory benson <victorybenson98@gmail.com>
2024-04-24 13:45:04 +0100

    minor updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,25 @@ import Image from "next/image";
 import linkedinIcon from "../../public/assets/linkedin.png";
 import facebookIcon from "../../public/assets/facebook.png";
 import twitterIcon from "../../public/assets/twitter.png";
+import Link from "next/link";
+import logo from "../../public/assets/footerLogo.png";
+
+
 export const Footer = () => {
   return (
     <section className="flex flex-col items-start justify-center gap-7 h-auto bg-[#0B195B] text-white py-8 md:px-10 px-5 ">
-      <div className="flex flex-wrap items-center w-full h-full justify-between md:px-8 space-y-4 ">
+      <div className="flex flex-wrap items-center w-full h-full justify-between space-y-4 ">
         <div className="gap-10">
-          <span className="mb-6 text-xl">LOGO</span>
-          <h3 className="leading-8 text-sm md:text-medium ">
-            Email:kemelevictory3802@gmail.com <br />
-            +2348140732549
-          </h3>
+            <p className="mb-6 text-xl ">
+            <Link href="/" className="flex items-center">
+              <Image src={logo} alt="logo" />
+              <span className="text-3xl font-bold">WBT</span>
+            </Link>
+          </p>
+          <p className="leading-8 text-sm md:text-medium ">
+            Email: willbethere@gmail.com <br />
+            Phone: +2348140732549
+          </p>
           <div className="flex items-start gap-2 mt-2">
             <Image
               src={linkedinIcon}
@@ -77,7 +86,7 @@ export const Footer = () => {
       </div>
       <div className="h-[1px] bg-white w-full md:w-[90%]"></div>
       <div className="text-sm flex flex-wrap gap-2 items-center ">
-        <p>&copy; 2023 positvus. All right reserved</p>
+        <p>&copy; 2024 will be there. All right reserved</p>
         <p>Privacy Policy</p>
       </div>
     </section>
--- src/components/sections/Overview2.jsx
+++ src/components/sections/Overview2.jsx
@@ -14,7 +14,7 @@ export const Overview2 = () => {
                     <Image src={image} alt={'image'} className='w-full h-full object-contain'/>
                 </div>
                 {/* right */}
-                <div className='space-y-4 md:w-[50%] order-first md:order-last'>
+                <div className='space-y-4 md:w-[50%] order-first md:order-last text-center md:text-start'>
                     <p className='font-bold sm:text-3xl text-xl'>Streamline your event journey from start to finish</p>
                     <p className='text-gray-500 text-lg'>
                         Begin with effortlessly compile a list of  for guests and items to contribute to your event, ensuring every detail is accounted for.
