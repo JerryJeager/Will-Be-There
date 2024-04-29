6a0492c35dbd334b8e6091d7edfd40e6b045e866
meet-tola <wasiusikiru7@gmail.com>
2024-04-29 15:57:13 +0100

    rsvp form new updates

--- app/invitation/[eventID]/page.tsx
+++ app/invitation/[eventID]/page.tsx
@@ -1,36 +1,69 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Page({ params }: { params: { eventID: string } }) {
   const searchParams = useSearchParams();
   const extras = searchParams.get("extras");
 
   interface FormData {
-    name: string;
+    first_name: string;
+    last_name: string;
     email: string;
     isAttending: "yes" | "no" | null;
-    hasExtras?: "yes" | "no" | null;
-    guests?: string[];
+    status: "attending" | "not_attending";
+    plus_ones?: { name: string }[];
+    event_id: string;
+    message: string;
   }
   const [formData, setFormData] = useState<FormData>({
-    name: "",
+    first_name: "",
+    last_name: "",
     email: "",
     isAttending: null,
-    hasExtras: null,
-    guests: Array.from({ length: Number(extras) }).map(() => ""),
+    status: "attending",
+    plus_ones: Array.from({ length: Number(extras) }).map(() => ({ name: "" })),
+    event_id: params.eventID,
+    message: "",
   });
+  const [eventData, setEventData] = useState<any>({});
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  useEffect(() => {
+    const fetchEventData = async () => {
+      try {
+        const token = sessionStorage.getItem('token');
+        if (!token) {
+          throw new Error('Access token is missing');
+        }
+
+        const response = await axios.get(
+          `https://will-be-there.onrender.com/api/v1/event/${params.eventID}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            },
+          }
+        );
+        setEventData(response.data);
+      } catch (error) {
+        console.error('Error fetching event data:', error);
+      }
+    };
+
+    fetchEventData();
+  }, [params.eventID]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
-    if (name.startsWith("guestName-")) {
+    if (name.startsWith("plusOneName-")) {
       const index = parseInt(name.split("-")[1]);
-      const newGuests = [...formData.guests];
-      newGuests[index] = value;
+      const newPlusOnes = [...formData.plus_ones];
+      newPlusOnes[index] = { name: value };
       setFormData((prevState) => ({
         ...prevState,
-        guests: newGuests,
+        plus_ones: newPlusOnes,
       }));
     } else {
       setFormData((prevState) => ({
@@ -40,31 +73,56 @@ export default function Page({ params }: { params: { eventID: string } }) {
     }
   };
 
-  const handleAddGuest = () => {
-    if (formData.guests.length < Number(extras)) {
+  const handleAddPlusOne = () => {
+    if (formData.plus_ones.length < Number(extras)) {
       setFormData((prevState) => ({
         ...prevState,
-        guests: [...prevState.guests, ""],
+        plus_ones: [...prevState.plus_ones, { name: "" }],
       }));
     }
   };
 
-  const handleRemoveGuest = (index: number) => {
-    const newGuests = [...formData.guests];
-    newGuests.splice(index, 1);
+  const handleRemovePlusOne = (index: number) => {
+    const newPlusOnes = [...formData.plus_ones];
+    newPlusOnes.splice(index, 1);
     setFormData((prevState) => ({
       ...prevState,
-      guests: newGuests,
+      plus_ones: newPlusOnes,
     }));
   };
-  const handleSubmit = (e: any) => {
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-    console.log(formData);
+    try {
+      const response = await axios.post(
+        `https://will-be-there.onrender.com/api/v1/invitation/guest?${params.eventID}`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${sessionStorage.getItem('token')}`
+          },
+        }
+      );
+      console.log('Form data submitted successfully');
+
+    } catch (error) {
+      console.error('Error submitting form data:', error);
+    }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
+      <div className="bg-white p-8 rounded-lg shadow-lg flex flex-col items-center">
+      <div className="mb-8">
+          {eventData && (
+            <div className="">
+            <img src={eventData.image_url} alt={eventData.name} className="mb-2 max-w-[300px]" />
+              <h2 className="text-2xl font-semibold text-gray-900">{eventData.name}</h2>
+              <p className="text-sm text-gray-600 text-center">{eventData.description}</p>
+            </div>
+          )}
+        </div>
         <form onSubmit={handleSubmit} className="space-y-6">
           <h2 className="text-2xl font-semibold text-gray-900">RSVP Form</h2>
           <p className="text-sm text-gray-600">
@@ -73,17 +131,32 @@ export default function Page({ params }: { params: { eventID: string } }) {
 
           <div className="grid grid-cols-1 gap-6">
             <div>
-              <label htmlFor="name" className="block text-sm font-medium text-gray-900">Name</label>
+              <label htmlFor="firstName" className="block text-sm font-medium text-gray-900">First Name</label>
               <input
                 type="text"
-                name="name"
-                id="name"
-                value={formData.name}
+                name="first_name"
+                id="firstName"
+                value={formData.first_name}
                 onChange={handleChange}
                 required
-                autoComplete="name"
+                autoComplete="given-name"
                 className="block w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none sm:text-sm"
-                placeholder="Enter your full name"
+                placeholder="Enter your first name"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="lastName" className="block text-sm font-medium text-gray-900">Last Name</label>
+              <input
+                id="lastName"
+                name="last_name"
+                type="text"
+                value={formData.last_name}
+                onChange={handleChange}
+                required
+                autoComplete="family-name"
+                className="block w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none sm:text-sm"
+                placeholder="Enter your last name"
               />
             </div>
 
@@ -141,10 +214,10 @@ export default function Page({ params }: { params: { eventID: string } }) {
                   <label htmlFor="yesExtras" className="flex items-center">
                     <input
                       id="yesExtras"
-                      name="hasExtras"
+                      name="status"
                       type="radio"
-                      value="yes"
-                      checked={formData.hasExtras === "yes"}
+                      value="attending"
+                      checked={formData.status === "attending"}
                       onChange={handleChange}
                       required
                       className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
@@ -154,10 +227,10 @@ export default function Page({ params }: { params: { eventID: string } }) {
                   <label htmlFor="noExtras" className="flex items-center">
                     <input
                       id="noExtras"
-                      name="hasExtras"
+                      name="status"
                       type="radio"
-                      value="no"
-                      checked={formData.hasExtras === "no"}
+                      value="not_attending"
+                      checked={formData.status === "not_attending"}
                       onChange={handleChange}
                       required
                       className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
@@ -168,23 +241,23 @@ export default function Page({ params }: { params: { eventID: string } }) {
               </fieldset>
             )}
 
-            {extras && Number(extras) !== 0 && formData.isAttending === "yes" && formData.hasExtras === "yes" && (
+            {extras && Number(extras) !== 0 && formData.isAttending === "yes" && formData.status === "attending" && (
               <div>
                 <h2 className="text-base font-semibold text-gray-900">Additional Guest Details</h2>
-                {formData.guests.map((guest, index) => (
+                {formData.plus_ones.map((plusOne, index) => (
                   <div key={index} className="flex items-center gap-4">
                     <input
                       type="text"
-                      name={`guestName-${index}`}
-                      value={guest}
+                      name={`plusOneName-${index}`}
+                      value={plusOne.name}
                       onChange={handleChange}
                       className="block w-full px-4 py-2 mb-3 mt-1 rounded-md border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none sm:text-sm"
-                      placeholder={`Guest ${index + 1} Name`}
+                      placeholder={`Plus One ${index + 1} Name`}
                     />
                     {index > 0 && (
                       <button
                         type="button"
-                        onClick={() => handleRemoveGuest(index)}
+                        onClick={() => handleRemovePlusOne(index)}
                         className="text-sm text-red-600 font-medium focus:outline-none"
                       >
                         Remove
@@ -192,19 +265,31 @@ export default function Page({ params }: { params: { eventID: string } }) {
                     )}
                   </div>
                 ))}
-                {formData.guests.length < Number(extras) && (
+                {formData.plus_ones.length < Number(extras) && (
                   <div className="mt-4">
                     <button
                       type="button"
-                      onClick={handleAddGuest}
+                      onClick={handleAddPlusOne}
                       className="bg-indigo-600 text-white px-4 py-2 text-[16px] rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
                     >
-                      Add Another Guest
+                      Add Another Plus One
                     </button>
                   </div>
                 )}
               </div>
             )}
+            <div>
+              <label htmlFor="message" className="block text-sm font-medium text-gray-900">Message</label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                className="block w-full px-4 py-2 rounded-md border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none sm:text-sm"
+                rows={4}
+                placeholder="Enter your message"
+              />
+            </div>
           </div>
 
           <div className="flex justify-end">
