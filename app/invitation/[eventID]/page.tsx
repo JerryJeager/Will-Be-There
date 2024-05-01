6f74098b1df27f01c6d3a64d78f161b61fc31e4c
Amadi Jerry <amadijerry310@gmail.com>
2024-05-01 03:07:18 +0100

    revert rsvp fix

--- app/invitation/[eventID]/page.tsx
+++ app/invitation/[eventID]/page.tsx
@@ -16,7 +16,7 @@ export default function Page({ params }: { params: { eventID: string } }) {
         last_name: string;
         email: string;
         isAttending: 'yes' | 'no' | null;
-        status: 'attending' | 'rejected';
+        status: 'attending' | 'not_attending';
         plus_ones?: { name: string }[];
         event_id: string;
         message: string;
@@ -73,14 +73,12 @@ export default function Page({ params }: { params: { eventID: string } }) {
           setFormData((prevState) => ({
               ...prevState,
               isAttending: value,
-              status: 'attending',
               plus_ones: []
           }));
       } else {
           // For other fields, update as usual
           setFormData((prevState) => ({
               ...prevState,
-              status: 'rejected',
               [name]: value
           }));
       }
@@ -119,7 +117,6 @@ export default function Page({ params }: { params: { eventID: string } }) {
                 }));
             }
         }
-        console.log(formData)
 
         try {
             const response = await axios.post(
@@ -137,17 +134,11 @@ export default function Page({ params }: { params: { eventID: string } }) {
 
 
             console.log('Form data submitted successfully');
-            
+            setShowCongratulations(true);
 
         } catch (error) {
             console.error('Error submitting form data:', error);
             alert("Your email has already been added, check your mail.")
-        }
-        finally{
-            if(formData.status == 'attending'){
-                setShowCongratulations(true);
-            }
-            router.push("/")
         }
     };
 
